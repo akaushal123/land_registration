919c2d065e9f25ee87da8c28a126386419f4381e
akaushal123 <akaushal123@gmail.com>
2020-05-23 12:42:57 +0530

    updated next.js removed parsing errors

--- ethereum/factory.js
+++ ethereum/factory.js
@@ -4,7 +4,7 @@ import LandRegistration from './build/LandRegistration.json';
 const address = '0x0dd48Ba4Dc9e058471e9B5F57B6A41ACbE345f69';
 
 const instance = new web3.eth.Contract(
-    JSON.parse(LandRegistration.interface),
+    LandRegistration.interface,
     address
 );
 
--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,17 @@
-import React from 'react';
+import React, { Component } from 'react';
+import factory from './../ethereum/factory';
+
+class Index extends Component {
+    async componentDidMount() {
+        const response = await factory.methods.viewAssets().call();
+        console.log(response);
+    }
+
+    render() {
+        return (
+            <div> Hello World Index! </div>
+        );
+    }
+}
+
+export default Index;
