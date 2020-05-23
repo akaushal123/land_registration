0cdf76f5f0e12a8f5810ea75bc0b78045088f856
Kaustubh Kislay <k.kislay20@gmail.com>
2020-05-23 13:59:12 +0530

    added layout

---
+++ components/Layout/Layout.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import Aux from './../../HOC/Aux';
+//import logo from './../../assets/img/tiranga_national_emblem.png';
+import styles from './Layout.module.css';
+
+class Layout extends Component {
+    render() {
+        return (
+            <Aux>
+                <div className={styles.Container}>
+                    <header className={styles.Header}>
+                        <div className={styles.LogoBox}>
+                            <img src="/static/tiranga_national_emblem.png" alt="logo" />
+                        </div>
+                        <div className={styles.NavBox}>
+                            <ul>
+                                <li> <a href="#"> Add Admin </a> </li>
+                                <li> <a href="#"> Explore </a> </li>
+                                <li> <a href="#"> Register Land </a> </li>
+                            </ul>
+                        </div>
+                    </header>
+                    <div className={styles.clearfix}></div>
+                    
+                    {this.props.children}
+                    
+                </div>
+            </Aux>
+        );
+    }
+}
+
+export default Layout;
---
+++ pages/superAdmin/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Layout from './../../components/Layout/Layout';
+
+class Index extends Component {
+    render() {
+        return (
+            <Layout>
+                <h1> Welcome!</h1>
+            </Layout>
+        );
+    }
+}
+
+export default Index;
